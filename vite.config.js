5b5f9daf4168ed109165bd7c647015b62002219a
Patt <patt.peng@gmail.com>
2023-12-24 00:17:54 +0700

    change base again

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+<<<<<<< HEAD
   base: '/react-weather-api/',
+=======
+>>>>>>> parent of 24443ce (change base on viteconfig)
 })
